9aaa4e46611724ce3373c480159a26e08625b6c4
DevanshuALW <sdevanshu096@gmail.com>
2025-11-10 18:06:10 +0530

    update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-        return "6363 Dockerized Flask App running successfully on AWS EC2!"
+        return "Dockerized Flask App running successfully on AWS EC2!"
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
